6fcf442fa3e9d19e1b673f670781ae42d58839c2
GuiOliv <guilherme.pinto.de.oliveira1@gmail.com>
2024-06-10 23:29:30 +0200

    [FEATURE] Added method to retreive JSON
     The HTML structure in `Chatbot.cshtml` was modified for better readability. A new file `JSONService.cs` was added with a new class `JSONService` and a struct `GreenhouseController`.

---
+++ Models/Services/JSONService.cs
@@ -0,0 +1,40 @@
+﻿using SF_DSS.Data;
+using SF_DSS.Data.Entities;
+
+namespace SF_DSS.Models.Services
+{
+    public class JSONService
+    {
+        #region PRIVATE FIELDS
+        private readonly HttpClient _httpClient;
+        #endregion
+
+        public JSONService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<GreenhouseController> GetResponse(Conversation conversation)
+        {
+            try
+            {
+                var response = await _httpClient.PostAsJsonAsync("http://95.99.2.118:5001/json_from_chat_history/", conversation.Messages);
+
+                return await response.Content.ReadFromJsonAsync<GreenhouseController>();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+
+    public struct GreenhouseController
+    {
+        public string Crop { get; set; }
+        public int Temperature { get; set; }
+        public int Humidity { get; set; }
+        public float Soil_ph { get; set; }
+    }
+}
--- obj/Debug/net8.0/SF-DSS.AssemblyInfo.cs
+++ obj/Debug/net8.0/SF-DSS.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SF-DSS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5004110501906d16cbe0e5866637212f4217df8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+169c9b34c38698956599da668106766cbc335029")]
 [assembly: System.Reflection.AssemblyProductAttribute("SF-DSS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SF-DSS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
