60dd1977046e3c9b8d26aee9e129a76707f6e08a
Guilherme.Oliveira <Guilherme.Oliveira@AEU-NBK-629>
2024-06-11 16:58:11 +0200

    [FEATURE] Connection between JSON and Chatbot
    In this commit, several significant changes were made to improve the chatbot functionality and user interface. An `HttpClient` instance was added to the `ChatbotController` to enable HTTP requests, and a `GetJsonResponse` method was introduced to fetch responses based on conversation details. The `Chatbot` method in `HomeController.cs` was modified to accept an optional `id` parameter, allowing for the retrieval of specific conversation details. A nullable `Conversation` property was added to the `ChatbotModel` class for including specific conversations in the model.
    
    In `JSONService.cs`, a timeout was added to the `HttpClient` instance to prevent indefinite waiting for a response. The `FunnyHaha` project was removed from the `SF-DSS.sln` solution.
    
    Several updates were made to the chat interface in `Chatbot.cshtml`, including the addition of an id to the "new chat" button, a `data-value` attribute to the chat area, and a script for redirecting users to specific conversation pages. The version of Font Awesome in `_Layout.cshtml` was updated and a script tag for jQuery was added.
    
    Finally, in `chat.js`, the "new chat" button now adds an event listener using its id, and the `sendMessage` function makes a GET request to either `GetResponse` or `GetJsonResponse` based on the user's message. If the message is "give me json", the chatbot responds with a table displaying the `GreenhouseController` response properties and a button for the user to send the information to the greenhouse.

--- Controllers/ChatbotController.cs
+++ Controllers/ChatbotController.cs
@@ -9,10 +9,12 @@ namespace SF_DSS.Controllers
     public class ChatbotController : Controller
     {
         private readonly IChatbotService _chatbotService;
+        private readonly HttpClient _httpClient;
 
-        public ChatbotController(IChatbotService chatbotService)
+        public ChatbotController(IChatbotService chatbotService, HttpClient httpClient)
         {
             _chatbotService = chatbotService;
+            _httpClient = httpClient;
         }
 
         public IActionResult Index()
@@ -25,6 +27,13 @@ namespace SF_DSS.Controllers
         {
             var response = await _chatbotService.GetResponse(message, convoId);
             return Ok(new Dictionary<string, object> { { "response", response.Messages.Last().Content }, { "id", response.ID } });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetJsonResponse(string message, int? convoId)
+        {
+            var response = await new JSONService(_httpClient).GetResponse(await _chatbotService.GetConversationDetails((int)convoId));
+            return Ok(response);
         }
 
         [HttpGet]
